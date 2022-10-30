28e7153a741ca1254415eba87ac750eb33390505
Sebastian Méndez Méndez <your_email_address@example.com>
2022-10-30 15:02:04 -0600

    adddepen

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,18 @@
 var gulp = require('gulp');
+var license = require('gulp-license');
+var pump = require('pump');
+
+
 gulp.task('default', function () { console.log('Hello Gulp!') });
+
+
+
+gulp.task('license', function (cb) {
+  pump([
+      gulp.src('./lib/*.js'),
+      license('MIT', {tiny: true}),
+      gulp.dest('./dist')
+    ],
+    cb
+  );
+});
